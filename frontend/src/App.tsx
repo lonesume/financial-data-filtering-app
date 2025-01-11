75cd225163e2d111ecf6e646e393b83e5ae4468c
Brian Joseph <brianjoseph1758@gmail.com>
2025-01-11 17:25:15 -0500

    ripped fetch call away to page.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,6 @@ import { useState } from "react";
 import "./App.css";
 import DemoPage from "./components/payments/page";
 function App() {
-  const URL = "/api/fetch-data";
-  fetch(URL)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
   return (
     <>
       <DemoPage />
